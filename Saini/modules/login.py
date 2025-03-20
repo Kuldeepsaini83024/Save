1530a8a7ddf253373030311286345421e3d76d61
Saini <Kuldeepsaini83024@gmail.com>
2025-03-20 22:38:22 +0530

    Update login.py

--- Saini/modules/login.py
+++ Saini/modules/login.py
@@ -1,25 +1,11 @@
-# ---------------------------------------------------
-# File Name: login.py
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
-# ---------------------------------------------------
-
 from pyrogram import filters, Client
-from devgagan import app
+from Saini import app  # ✅ Fixed
 import random
 import os
 import asyncio
 import string
-from devgagan.core.mongo import db
-from devgagan.core.func import subscribe, chk_user
+from Saini.core.mongo import db  # ✅ Fixed
+from Saini.core.func import subscribe, chk_user  # ✅ Fixed
 from config import API_ID as api_id, API_HASH as api_hash
 from pyrogram.errors import (
     ApiIdInvalid,
